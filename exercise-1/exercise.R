655fab2d4deaa695140f5cadd1e025c2b0069695
Paul Vaden <pvaden@uw.edu>
2017-01-10 15:20:36 -0800

    In class work

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,10 +2,12 @@
 
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-
+AddThree <- function(input){
+  return(input + 3)
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
